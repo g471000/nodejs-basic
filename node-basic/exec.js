b8cab541829ff4e4bce5ca5408bacca54fa93b94
jieun chon <g471000@gmail.com>
2022-01-09 22:48:26 +0900

    using child process

--- node-basic/exec.js
+++ node-basic/exec.js
@@ -3,10 +3,9 @@ const exec = require('child_process').exec;
 const process = exec('ls');
 
 process.stdout.on('data', function (data) {
-    console.log(data.toString());
+    console.log(data.toString('utf8'));
 });
 
 process.stderr.on('data', function (data) {
-    console.error(data.toString());
+    console.error(data.toString('utf8'));
 });
-
