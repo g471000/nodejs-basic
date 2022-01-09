e0803fec6d3cc50b28ac131500cd5fa55f1aabcb
jieun chon <g471000@gmail.com>
2022-01-09 22:38:15 +0900

    count number of primes using multiple threads

--- node-basic/prime.js
+++ node-basic/prime.js
@@ -1,26 +1,58 @@
-const min = 2;
-const max = 10_000_000;
-const primes = [];
+const {Worker, isMainThread, parentPort, workerData} = require('worker_threads');
 
-function generatePrimes(start, range) {
+const min = 2;
+let primes = [];
+
+function findPrimes(start, range) {
     let isPrime = true;
     const end = start + range;
     for (let i = start; i < end; i++) {
         for (let j = min; j < Math.sqrt(end); j++) {
-            if (i !== j && i % j ===0) {
+            if (i !== j && i % j === 0) {
                 isPrime = false;
                 break;
             }
         }
         if (isPrime) {
-            console.log(i, "is a prime number.")
             primes.push(i);
         }
         isPrime = true;
     }
 }
 
-console.time('prime');
-generatePrimes(min, max);
-console.timeEnd('prime');
-console.log(primes.length);
+if (isMainThread) {
+    const max = 10_000_000;
+    const threadCount = 8;
+    const threads = new Set();
+    const range = Math.ceil((max - min) / threadCount);
+    let start = min;
+    console.time('prime');
+    for (let i = 0; i < threadCount - 1; i++) {
+        const wStart = start;
+        threads.add(new Worker(__filename, {workerData: {start: wStart, range}}));
+        start += range;
+    }
+    threads.add(new Worker(__filename, {workerData: {start, range: range + ((max - min + 1) % threadCount)}}));
+
+    for (let worker of threads) {
+        worker.on('error', (err) => {
+            throw err;
+        });
+
+        worker.on('exit', () => {
+            threads.delete(worker);
+            if (threads.size === 0) {
+                console.timeEnd('prime');
+                console.log(primes.length);
+            }
+        });
+        worker.on('message', (msg) => {
+            primes = primes.concat(msg);
+        })
+    }
+    console.log(primes.length);
+} else {
+    findPrimes(workerData.start, workerData.range);
+    parentPort.postMessage(primes);
+}
+
