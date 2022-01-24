4be29f75864c06addccbd54368ce168338e850b9
jieun chon <g471000@gmail.com>
2022-01-25 00:22:30 +0900

    using pug

--- express/learn-express/app.js
+++ express/learn-express/app.js
@@ -14,6 +14,9 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(morgan('combined'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
